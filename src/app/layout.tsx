766d653a13ded0156a5eb6380c06022c2e3de76e
Serhiy Lovchinsky <s.lovchinsky@goit.ua>
2024-01-11 15:29:15 +0200

    Add layout with sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,17 @@
-import { PropsWithChildren } from 'react';
-import NavLink from '@/app/components/nav-link';
+import React from 'react';
+import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
 
-export default function Layout({ children }: PropsWithChildren) {
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={'bg-[#fafafa] flex flex-col items-center h-screen'}>
-        <header
-          className={
-            'flex justify-center items-center min-h-12 bg-blue-400 w-full'
-          }>
-          <nav className={'flex gap-5 uppercase'}>
-            <NavLink path={'/'}>Home</NavLink>
-            <NavLink path={'/companies'}>Companies</NavLink>
-          </nav>
-        </header>
-        <main className={'flex-grow w-full'}>{children}</main>
-        <footer
-          className={
-            'flex justify-center items-center min-h-12 bg-blue-400 w-full'
-          }>
-          I am footer with something noone cares about
-        </footer>
-      </body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
