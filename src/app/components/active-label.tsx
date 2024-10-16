bb096e4025a902f7a061723b061726a715df6a93
spalnique <lex.sydoruk@gmail.com>
2024-10-17 01:50:16 +0200

    added StatusLabel, tailwind applied

---
+++ src/app/components/active-label.tsx
@@ -0,0 +1,26 @@
+export enum Status {
+  Active = 'Active',
+  NotActive = 'Not Active',
+  Pending = 'Pending',
+  Suspended = 'Suspended',
+}
+
+export type StatusLabelProps = { status: Status };
+
+function StatusLabel({ status }: StatusLabelProps) {
+  return (
+    <div
+      className={`inline-flex items-center px-3.5 py-1 rounded-3xl cursor-default text-sm font-medium ${
+        (status === Status.Active && 'text-green-700 bg-green-100') ||
+        (status === Status.NotActive && 'text-red-700 bg-red-100') ||
+        (status === Status.Pending && 'text-orange-700 bg-orange-100') ||
+        (status === Status.Suspended && 'text-blue-700 bg-blue-100') ||
+        ''
+      }`}>
+      <span className={'size-1 mr-2 rounded-full bg-current'} />
+      {status}
+    </div>
+  );
+}
+
+export default StatusLabel;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from '@/app/components/active-label';
+
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
+      <StatusLabel status={Status.Active} />
+      <StatusLabel status={Status.NotActive} />
+      <StatusLabel status={Status.Pending} />
+      <StatusLabel status={Status.Suspended} />
     </main>
   );
 }
