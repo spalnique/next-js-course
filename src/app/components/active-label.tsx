cee604b95e36766c8ed2b02eaef3539714e2ade9
spalnique <lex.sydoruk@gmail.com>
2024-10-17 13:18:13 +0200

    Static and Dynamic Rendering

--- src/app/components/active-label.tsx
+++ src/app/components/active-label.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+
 export enum Status {
   Active = 'Active',
   NotActive = 'Not Active',
@@ -5,21 +7,24 @@ export enum Status {
   Suspended = 'Suspended',
 }
 
-export type StatusLabelProps = { status: Status };
+export type StatusLabelProps = { status: Status; disabled?: boolean };
 
-function StatusLabel({ status }: StatusLabelProps) {
+function StatusLabel({ status, disabled }: StatusLabelProps) {
   return (
-    <div
-      className={`inline-flex items-center px-3.5 py-1 rounded-3xl cursor-default text-sm font-medium ${
-        (status === Status.Active && 'text-green-700 bg-green-100') ||
-        (status === Status.NotActive && 'text-red-700 bg-red-100') ||
-        (status === Status.Pending && 'text-orange-700 bg-orange-100') ||
-        (status === Status.Suspended && 'text-blue-700 bg-blue-100') ||
-        ''
-      }`}>
-      <span className={'size-1 mr-2 rounded-full bg-current'} />
-      {status}
-    </div>
+    <>
+      <div
+        className={clsx(
+          'inline-flex items-center px-3.5 py-1 rounded-3xl cursor-default text-sm font-medium',
+          status === Status.Active && 'text-green-700 bg-green-100',
+          status === Status.NotActive && 'text-red-700 bg-red-100',
+          status === Status.Pending && 'text-orange-700 bg-orange-100',
+          status === Status.Suspended && 'text-blue-700 bg-blue-100',
+          { ['opacity-75 cursor-not-allowed']: disabled }
+        )}>
+        <span className={'size-1 mr-2 rounded-full bg-current'} />
+        {status}
+      </div>
+    </>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import StatusLabel, { Status } from '@/app/components/active-label';
+import { headers } from 'next/headers';
 
 export default function Home() {
+  headers();
   return (
     <main>
       <h1>Home page</h1>
+      <p>{new Date().toTimeString()}</p>
       <StatusLabel status={Status.Active} />
       <StatusLabel status={Status.NotActive} />
       <StatusLabel status={Status.Pending} />
